cd13423066fcbccc7e6fce7d57f2d53c0d0bab37
vagrant <vagrant@precise32.(none)>
2016-06-30 10:10:35 +0000

    Add user controller and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Plantswap::Application.routes.draw do
   resources :plants do
     resources :photos
   end
-  resources :users
+  resources :users, only: :show
 
 
   # The priority is based upon order of creation: first created -> highest priority.
